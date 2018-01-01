aaa8b1afa0243ac8e872762daf56ee8d1bea0c62
StefMa <StefMaDev@outlook.com>
2018-01-01 13:54:04 +0100

    Use the power of Kotlin \o/

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,18 +12,10 @@ dependencies {
 }
 
 gradlePlugin {
-  plugins(object: Action<NamedDomainObjectContainer<PluginDeclaration>> {
-
-    override fun execute(pluginDeclarationContainer: NamedDomainObjectContainer<PluginDeclaration>) {
-      pluginDeclarationContainer.create("joka", object: Action<PluginDeclaration> {
-
-        override fun execute(pluginDeclaration: PluginDeclaration) {
-          pluginDeclaration.setId("guru.stefma.joka")
-          pluginDeclaration.setImplementationClass("guru.stefma.joka.JokaPlugin")
-        }
-
-      })
+  plugins {
+    create("joka") {
+      id = "guru.stefma.joka"
+      implementationClass = "guru.stefma.joka.JokaPlugin"
     }
-
-  })
+  }
 }
