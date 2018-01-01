90940096975fd19e990f4aee241bcf512471ff48
StefMa <StefMaDev@outlook.com>
2018-01-01 13:50:20 +0100

    Use java-gradle-plugin for easier development

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
   kotlin("jvm") version("1.2.10")
+  id("java-gradle-plugin")
 }
 
 repositories {
@@ -8,4 +9,21 @@ repositories {
 
 dependencies {
   compile(kotlin("stdlib"))
+}
+
+gradlePlugin {
+  plugins(object: Action<NamedDomainObjectContainer<PluginDeclaration>> {
+
+    override fun execute(pluginDeclarationContainer: NamedDomainObjectContainer<PluginDeclaration>) {
+      pluginDeclarationContainer.create("joka", object: Action<PluginDeclaration> {
+
+        override fun execute(pluginDeclaration: PluginDeclaration) {
+          pluginDeclaration.setId("guru.stefma.joka")
+          pluginDeclaration.setImplementationClass("guru.stefma.joka.JokaPlugin")
+        }
+
+      })
+    }
+
+  })
 }
